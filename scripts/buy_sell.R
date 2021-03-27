1c4d750cdd521ddbc06d57dfb8a5e1ce2363340c
Nicholas C. Rivera <riveranicholasc@gmail.com>
2021-03-27 11:26:00 -0400

    Modifications to the loops.

--- scripts/buy_sell.R
+++ scripts/buy_sell.R
@@ -4,17 +4,18 @@ library(lubridate)
 
 
 # crypto_tickers <- read.csv(file = "data/crypto-currency.csv")
-crypto_tickers <- c("BTC", "ETH", "USDT", "BNB", "ADA", "DOT", "XRP", "UNI", "LTC", "THETA", "LINK", "USDC", "BCH", "XLM", "KLAY", "FIL", "WBTC", "LUNA", "DOGE", "VET", "CRO", "TRX", "AAVE", "SOL", "KSM", "ATOM", "XMR", "MIOTA", "BTT", "EOS", "BSV", "AVAX", "BUSD", "FTT", "XTZ", "XEM", "ALGO", "DAI", "NEO", "CHZ", "HBAR", "EGLD", "HT", "CAKE", "LEO", "TFUEL", "SUSHI", "DCR", "MKR", "DASH", "SNX", "NPXS", "ENJ", "GRT", "BTMX", "ZIL", "NEAR", "HOT", "MATIC", "COMP", "RVN", "ONE", "BAT", "BTCB", "UST", "ZEC", "NEXO", "RUNE", "MANA", "ETC", "STX", "UMA", "ICX", "BNT", "WAVES", "CHSB", "CEL", "YFI", "ANKR", "VGX", "ONT", "REV", "ZRX", "RSR", "SC", "DENT", "DGB", "REN", "FTM", "FLOW", "AR", "PAX", "QTUM", "IOST", "OKB", "OMG", "HNT", "HUSD", "CRV", "RENBTC", "CELO", "VTHO", "CKB", "BTG", "LRC", "NANO", "OCEAN", "1INCH", "MVL", "LSK", "GLM", "STORJ", "LPT", "ZEN", "FET", "KNC", "HEDG", "XVG", "QNT", "ZKS", "STMX", "ALPHA", "EWT", "ORBS", "CELR", "SNT", "REEF", "SKL", "KCS", "ANT", "XVS", "BAL", "DODO", "IOTX", "WAXP", "REP", "FUN", "POLY", "KIN", "TEL", "TUSD", "SWAP", "BADGER", "KAVA", "COTI", "CVC", "BORA", "MAID", "ARDR", "META", "VLX", "QKC", "STEEM", "SXP", "BAND", "TRAC", "OXT", "KMD", "OGN", "AGI", "IQ", "UTK", "MED", "ARK", "ROSE", "RIF", "MFT", "POLS", "SRM", "ALICE", "NU", "CVT", "NMR", "AVA", "XHV", "GNO", "BTS", "SPND", "IRIS", "WAN", "AMPL", "ELF", "HIVE", "JST", "BCD", "WOO", "TOMO", "NOIA", "STRAX", "LTO", "RDD", "SYS", "CRE", "SHR", "UOS", "SCRT", "ZB", "POWR", "XOR", "LIT")
+# crypto_tickers <- c("BTC", "ETH", "USDT", "BNB", "ADA", "DOT", "XRP", "UNI", "LTC", "THETA", "LINK", "USDC", "BCH", "XLM", "KLAY", "FIL", "WBTC", "LUNA", "DOGE", "VET", "CRO", "TRX", "AAVE", "SOL", "KSM", "ATOM", "XMR", "MIOTA", "BTT", "EOS", "BSV", "AVAX", "BUSD", "FTT", "XTZ", "XEM", "ALGO", "DAI", "NEO", "CHZ", "HBAR", "EGLD", "HT", "CAKE", "LEO", "TFUEL", "SUSHI", "DCR", "MKR", "DASH", "SNX", "NPXS", "ENJ", "GRT", "BTMX", "ZIL", "NEAR", "HOT", "MATIC", "COMP", "RVN", "ONE", "BAT", "BTCB", "UST", "ZEC", "NEXO", "RUNE", "MANA", "ETC", "STX", "UMA", "ICX", "BNT", "WAVES", "CHSB", "CEL", "YFI", "ANKR", "VGX", "ONT", "REV", "ZRX", "RSR", "SC", "DENT", "DGB", "REN", "FTM", "FLOW", "AR", "PAX", "QTUM", "IOST", "OKB", "OMG", "HNT", "HUSD", "CRV", "RENBTC", "CELO", "VTHO", "CKB", "BTG", "LRC", "NANO", "OCEAN", "1INCH", "MVL", "LSK", "GLM", "STORJ", "LPT", "ZEN", "FET", "KNC", "HEDG", "XVG", "QNT", "ZKS", "STMX", "ALPHA", "EWT", "ORBS", "CELR", "SNT", "REEF", "SKL", "KCS", "ANT", "XVS", "BAL", "DODO", "IOTX", "WAXP", "REP", "FUN", "POLY", "KIN", "TEL", "TUSD", "SWAP", "BADGER", "KAVA", "COTI", "CVC", "BORA", "MAID", "ARDR", "META", "VLX", "QKC", "STEEM", "SXP", "BAND", "TRAC", "OXT", "KMD", "OGN", "AGI", "IQ", "UTK", "MED", "ARK", "ROSE", "RIF", "MFT", "POLS", "SRM", "ALICE", "NU", "CVT", "NMR", "AVA", "XHV", "GNO", "BTS", "SPND", "IRIS", "WAN", "AMPL", "ELF", "HIVE", "JST", "BCD", "WOO", "TOMO", "NOIA", "STRAX", "LTO", "RDD", "SYS", "CRE", "SHR", "UOS", "SCRT", "ZB", "POWR", "XOR", "LIT")
+# crypto_tickers <- c("BTC", "ETH")
 
 
 
 API_BUY <- function(Ticker){
     x <- str_glue(
-        # "https://bigdataforall.com/stocks/?",
         "https://bigdataforall.com/crypto/?",
-        # "key=pogba&tiingo_key=bb4ce17dee09aa67599085bde6f6c0e420aa5d5f",
-        "key=9a77c771-8e8c-11eb-adad-da11bc1fe3d8",
-        "&request=buy&symbol={Ticker}&quantity=.025",
+        # "key=4070687b-8e7d-11eb-adad-da11bc1fe3d8", # XIO KEY
+        # "key=a788a434-8e80-11eb-adad-da11bc1fe3d8", # LILIA KEY
+        "key=9a77c771-8e8c-11eb-adad-da11bc1fe3d8", # TEAM KEY
+        "&request=buy&symbol={Ticker}&quantity=.5",
         sep = ""
     )
     return(x)
@@ -22,33 +23,34 @@ API_BUY <- function(Ticker){
 
 
 API_SELL <- function(Ticker){
-    x <- SellURL <- str_glue(
-        "https://bigdataforall.com/stocks/?",
-        "key=pogba&tiingo_key=bb4ce17dee09aa67599085bde6f6c0e420aa5d5f",
-        "&request=sell&symbol={Ticker}&quantity=3",
+    x <- str_glue(
+        "https://bigdataforall.com/crypto/?",
+        # "key=a788a434-8e80-11eb-adad-da11bc1fe3d8", # LILIA KEY
+        # "key=9a77c771-8e8c-11eb-adad-da11bc1fe3d8", # XIO
+        "key=9a77c771-8e8c-11eb-adad-da11bc1fe3d8",
+        "&request=sell&symbol={Ticker}&quantity=.5",
         sep = ""
     )
     return(x)
 }
 
 
-Dataset <- Dataset %>% 
-    mutate(API_BUY = Ticker %>% map_chr(API_BUY), 
-           API_SELL = Ticker %>% map_chr(API_SELL))
-
-loop_vector <- Dataset %>% pull(API_BUY) %>% c()
-
-# buy_data <- fromJSON(BuyURL, flatten = TRUE)
-
-# sell_url <- fromJSON(SellURL, flatten = TRUE)
-
-#BUY
-for (tckr in loop_vector) {
-    x <- fromJSON(tckr)
+while (tckr == "BTC") {
+    print("-----------------------BUY---------------------")
+    x <- fromJSON(API_BUY(tckr), flatten = TRUE)
     print(tckr)
     print(x)
-    Sys.sleep(30)
+    Sys.sleep(10)
+    
+    print("++++++++++++++++++++++++SELL++++++++++++++++++++++")
+    y <- fromJSON(API_SELL(tckr), flatten = TRUE)
+    print(tckr)
+    print(y)
+    Sys.sleep(10)
+    
 }
 
 
+# fromJSON("https://bigdataforall.com/crypto/?key=4070687b-8e7d-11eb-adad-da11bc1fe3d8&request=reset")
+# fromJSON("https://bigdataforall.com/crypto/?key=a788a434-8e80-11eb-adad-da11bc1fe3d8&request=reset")
 
