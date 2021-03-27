073f3f2e27cfca0fed328af901be3455f458b194
Nicholas C. Rivera <riveranicholasc@gmail.com>
2021-03-27 09:43:32 -0400

    Modification - Ticerks up top.

--- scripts/buy_sell.R
+++ scripts/buy_sell.R
@@ -1,15 +1,10 @@
-library(tidyr)
-library(purrr)
+library(tidyverse)
 library(jsonlite)
-library(stringr)
-library(dplyr)
 library(lubridate)
 
 
-MarketData <- read.csv(file = "data/top100.csv")
-
-Dataset <- MarketData %>% select(Ticker)
-
+# crypto_tickers <- read.csv(file = "data/crypto-currency.csv")
+crypto_tickers <- c("BTC", "ETH", "USDT", "BNB", "ADA", "DOT", "XRP", "UNI", "LTC", "THETA", "LINK", "USDC", "BCH", "XLM", "KLAY", "FIL", "WBTC", "LUNA", "DOGE", "VET", "CRO", "TRX", "AAVE", "SOL", "KSM", "ATOM", "XMR", "MIOTA", "BTT", "EOS", "BSV", "AVAX", "BUSD", "FTT", "XTZ", "XEM", "ALGO", "DAI", "NEO", "CHZ", "HBAR", "EGLD", "HT", "CAKE", "LEO", "TFUEL", "SUSHI", "DCR", "MKR", "DASH", "SNX", "NPXS", "ENJ", "GRT", "BTMX", "ZIL", "NEAR", "HOT", "MATIC", "COMP", "RVN", "ONE", "BAT", "BTCB", "UST", "ZEC", "NEXO", "RUNE", "MANA", "ETC", "STX", "UMA", "ICX", "BNT", "WAVES", "CHSB", "CEL", "YFI", "ANKR", "VGX", "ONT", "REV", "ZRX", "RSR", "SC", "DENT", "DGB", "REN", "FTM", "FLOW", "AR", "PAX", "QTUM", "IOST", "OKB", "OMG", "HNT", "HUSD", "CRV", "RENBTC", "CELO", "VTHO", "CKB", "BTG", "LRC", "NANO", "OCEAN", "1INCH", "MVL", "LSK", "GLM", "STORJ", "LPT", "ZEN", "FET", "KNC", "HEDG", "XVG", "QNT", "ZKS", "STMX", "ALPHA", "EWT", "ORBS", "CELR", "SNT", "REEF", "SKL", "KCS", "ANT", "XVS", "BAL", "DODO", "IOTX", "WAXP", "REP", "FUN", "POLY", "KIN", "TEL", "TUSD", "SWAP", "BADGER", "KAVA", "COTI", "CVC", "BORA", "MAID", "ARDR", "META", "VLX", "QKC", "STEEM", "SXP", "BAND", "TRAC", "OXT", "KMD", "OGN", "AGI", "IQ", "UTK", "MED", "ARK", "ROSE", "RIF", "MFT", "POLS", "SRM", "ALICE", "NU", "CVT", "NMR", "AVA", "XHV", "GNO", "BTS", "SPND", "IRIS", "WAN", "AMPL", "ELF", "HIVE", "JST", "BCD", "WOO", "TOMO", "NOIA", "STRAX", "LTO", "RDD", "SYS", "CRE", "SHR", "UOS", "SCRT", "ZB", "POWR", "XOR", "LIT")
 
 API_BUY <- function(Ticker){
     x <- str_glue(
@@ -51,5 +46,4 @@ for (tckr in loop_vector) {
 }
 
 
-crypto_tickers <- c("BTC", "ETH", "USDT", "BNB", "ADA", "DOT", "XRP", "UNI", "LTC", "THETA", "LINK", "USDC", "BCH", "XLM", "KLAY", "FIL", "WBTC", "LUNA", "DOGE", "VET", "CRO", "TRX", "AAVE", "SOL", "KSM", "ATOM", "XMR", "MIOTA", "BTT", "EOS", "BSV", "AVAX", "BUSD", "FTT", "XTZ", "XEM", "ALGO", "DAI", "NEO", "CHZ", "HBAR", "EGLD", "HT", "CAKE", "LEO", "TFUEL", "SUSHI", "DCR", "MKR", "DASH", "SNX", "NPXS", "ENJ", "GRT", "BTMX", "ZIL", "NEAR", "HOT", "MATIC", "COMP", "RVN", "ONE", "BAT", "BTCB", "UST", "ZEC", "NEXO", "RUNE", "MANA", "ETC", "STX", "UMA", "ICX", "BNT", "WAVES", "CHSB", "CEL", "YFI", "ANKR", "VGX", "ONT", "REV", "ZRX", "RSR", "SC", "DENT", "DGB", "REN", "FTM", "FLOW", "AR", "PAX", "QTUM", "IOST", "OKB", "OMG", "HNT", "HUSD", "CRV", "RENBTC", "CELO", "VTHO", "CKB", "BTG", "LRC", "NANO", "OCEAN", "1INCH", "MVL", "LSK", "GLM", "STORJ", "LPT", "ZEN", "FET", "KNC", "HEDG", "XVG", "QNT", "ZKS", "STMX", "ALPHA", "EWT", "ORBS", "CELR", "SNT", "REEF", "SKL", "KCS", "ANT", "XVS", "BAL", "DODO", "IOTX", "WAXP", "REP", "FUN", "POLY", "KIN", "TEL", "TUSD", "SWAP", "BADGER", "KAVA", "COTI", "CVC", "BORA", "MAID", "ARDR", "META", "VLX", "QKC", "STEEM", "SXP", "BAND", "TRAC", "OXT", "KMD", "OGN", "AGI", "IQ", "UTK", "MED", "ARK", "ROSE", "RIF", "MFT", "POLS", "SRM", "ALICE", "NU", "CVT", "NMR", "AVA", "XHV", "GNO", "BTS", "SPND", "IRIS", "WAN", "AMPL", "ELF", "HIVE", "JST", "BCD", "WOO", "TOMO", "NOIA", "STRAX", "LTO", "RDD", "SYS", "CRE", "SHR", "UOS", "SCRT", "ZB", "POWR", "XOR", "LIT")
-write.csv(x = crypto_tickers, file = "data/crypto-currency.csv")
+
