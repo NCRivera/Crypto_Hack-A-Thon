24af70f739723213b2b2e527b97ddfc1d44cbf15
Nicholas C. Rivera <riveranicholasc@gmail.com>
2021-03-27 10:08:31 -0400

    Modify function to buy.

--- scripts/buy_sell.R
+++ scripts/buy_sell.R
@@ -6,15 +6,20 @@ library(lubridate)
 # crypto_tickers <- read.csv(file = "data/crypto-currency.csv")
 crypto_tickers <- c("BTC", "ETH", "USDT", "BNB", "ADA", "DOT", "XRP", "UNI", "LTC", "THETA", "LINK", "USDC", "BCH", "XLM", "KLAY", "FIL", "WBTC", "LUNA", "DOGE", "VET", "CRO", "TRX", "AAVE", "SOL", "KSM", "ATOM", "XMR", "MIOTA", "BTT", "EOS", "BSV", "AVAX", "BUSD", "FTT", "XTZ", "XEM", "ALGO", "DAI", "NEO", "CHZ", "HBAR", "EGLD", "HT", "CAKE", "LEO", "TFUEL", "SUSHI", "DCR", "MKR", "DASH", "SNX", "NPXS", "ENJ", "GRT", "BTMX", "ZIL", "NEAR", "HOT", "MATIC", "COMP", "RVN", "ONE", "BAT", "BTCB", "UST", "ZEC", "NEXO", "RUNE", "MANA", "ETC", "STX", "UMA", "ICX", "BNT", "WAVES", "CHSB", "CEL", "YFI", "ANKR", "VGX", "ONT", "REV", "ZRX", "RSR", "SC", "DENT", "DGB", "REN", "FTM", "FLOW", "AR", "PAX", "QTUM", "IOST", "OKB", "OMG", "HNT", "HUSD", "CRV", "RENBTC", "CELO", "VTHO", "CKB", "BTG", "LRC", "NANO", "OCEAN", "1INCH", "MVL", "LSK", "GLM", "STORJ", "LPT", "ZEN", "FET", "KNC", "HEDG", "XVG", "QNT", "ZKS", "STMX", "ALPHA", "EWT", "ORBS", "CELR", "SNT", "REEF", "SKL", "KCS", "ANT", "XVS", "BAL", "DODO", "IOTX", "WAXP", "REP", "FUN", "POLY", "KIN", "TEL", "TUSD", "SWAP", "BADGER", "KAVA", "COTI", "CVC", "BORA", "MAID", "ARDR", "META", "VLX", "QKC", "STEEM", "SXP", "BAND", "TRAC", "OXT", "KMD", "OGN", "AGI", "IQ", "UTK", "MED", "ARK", "ROSE", "RIF", "MFT", "POLS", "SRM", "ALICE", "NU", "CVT", "NMR", "AVA", "XHV", "GNO", "BTS", "SPND", "IRIS", "WAN", "AMPL", "ELF", "HIVE", "JST", "BCD", "WOO", "TOMO", "NOIA", "STRAX", "LTO", "RDD", "SYS", "CRE", "SHR", "UOS", "SCRT", "ZB", "POWR", "XOR", "LIT")
 
+
+
 API_BUY <- function(Ticker){
     x <- str_glue(
-        "https://bigdataforall.com/stocks/?",
-        "key=pogba&tiingo_key=bb4ce17dee09aa67599085bde6f6c0e420aa5d5f",
-        "&request=buy&symbol={Ticker}&quantity=6",
+        # "https://bigdataforall.com/stocks/?",
+        "https://bigdataforall.com/crypto/?",
+        # "key=pogba&tiingo_key=bb4ce17dee09aa67599085bde6f6c0e420aa5d5f",
+        "key=9a77c771-8e8c-11eb-adad-da11bc1fe3d8",
+        "&request=buy&symbol={Ticker}&quantity=.025",
         sep = ""
     )
     return(x)
 }
+
 
 API_SELL <- function(Ticker){
     x <- SellURL <- str_glue(
